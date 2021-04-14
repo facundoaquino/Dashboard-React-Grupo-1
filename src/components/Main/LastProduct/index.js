d118c6a581a45bd7f6f7e394194a956dd0f8ab1b
tomasVergara14 <tomasvergara272@gmail.com>
2021-04-14 11:37:36 -0300

    detalles

--- src/components/Main/LastProduct/index.js
+++ src/components/Main/LastProduct/index.js
@@ -16,8 +16,8 @@ const LastProduct = (props)=>{
 						<div className="text-center">
 							<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: "25rem"}}  src={`http://localhost:3050/${props.image}`}  alt="El usuario no posee foto" />
 						</div>
-						<p>El ultimo es {props.name} {props.lastname}</p>
-						<p className="providerDetail " > <u>Detalles del Ultimo proveedor</u> </p>
+						<p>El ultimo: {props.name} {props.lastname}</p>
+						<p className="providerDetail " > <u>Contacto del Ultimo proveedor</u> </p>
                         <div id="Details" className=" unShowDetails showDetails"  > 
                             <ul>
                                 <li>{props.location} </li>
